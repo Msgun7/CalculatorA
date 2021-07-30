6ab108c74722519fdc32b5fa6fdf2c28e1a229b6
MSgun7 <fgarr@naver.com>
2021-07-30 23:20:53 +0900

    Added calculator source.

---
+++ Source/Calculator1/Calculator1App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator1App.swift
+//  Calculator1
+//
+//  Created by Jaegeon Yeon on 2021/07/28.
+//
+
+import SwiftUI
+
+@main
+struct Calculator1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
